b9f2b00141c86986dff5215f064f6be47c49ca86
Ruan Montelo <ruanmontelo@gmail.com>
2019-11-24 02:00:05 -0300

    One more test has been implemented for the treatErrorMessage command in utils.js

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -2,8 +2,13 @@ const utils = require('../commands/utils');
 
 let error = "Cannot read property x of undefined";
 
+let errorMessage = {
+    message: "Unhandled exception."
+}
+
 test('Needs to return what is inside an error message or itself', () => {
     expect(utils.treatErrorMessage(error)).toBeDefined();
+    expect(utils.treatErrorMessage(errorMessage.message)).toBeDefined();
 });
 
 test('Return the comparison of type and values of two different objects', () => {

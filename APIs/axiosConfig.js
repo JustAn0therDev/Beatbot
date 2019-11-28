94092e5549743c20aaed650e87c2c37160f1ee7d
Ruan Montelo <ruanmontelo@gmail.com>
2019-11-28 01:24:18 -0300

    Axios config file has been removed from the project.

--- APIs/axiosConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
-}
